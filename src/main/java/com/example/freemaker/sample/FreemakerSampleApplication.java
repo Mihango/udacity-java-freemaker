692a702aad05824ca83f01783f5b9c2d593bccee
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-02 17:58:09 +0300

    Free maker sample

---
+++ src/main/java/com/example/freemaker/sample/FreemakerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.freemaker.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemakerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemakerSampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/freemaker/sample/models/Student.java
@@ -0,0 +1,38 @@
+package com.example.freemaker.sample.models;
+
+public class Student {
+
+    private Integer id;
+    private String name;
+    private double grade;
+
+    public Student(Integer id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
